00d30875aab762039db72fba51fb1a0390cf9159
Luiz Claudio <luiz.claudiolc@outlook.com>
2026-08-15 13:52:04 -0300

    Auto-generated commit

---
+++ chunk-FQ3IA6LY.js
@@ -0,0 +1,1 @@
+import{A as i,_a as a,h as o,x as n}from"./chunk-K7FTLC5L.js";var c=class r{http=i(a);buscarCep(t){let e=t.replace(/\D/g,"");return e.length!==8?o(null):this.http.get(`https://viacep.com.br/ws/${e}/json/`)}static \u0275fac=function(e){return new(e||r)};static \u0275prov=n({token:r,factory:r.\u0275fac,providedIn:"root"})};export{c as a};
